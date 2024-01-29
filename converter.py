87dc62fc4c8fb3e0dda26b8ede26482efc5d9d64
Jose Abzum <jose.abzum@escala24x7.com>
2024-01-29 11:03:25 -0600

    testing spaces on parameters

---
+++ converter.py
@@ -0,0 +1,11 @@
+import json
+
+# Lee el contenido del archivo JSON
+with open('parameters.json', 'r') as file:
+    data = json.load(file)
+
+# Crea la cadena de texto en formato bash
+bash_array = " ".join([f"'{item['ParameterKey']}=\"{item['ParameterValue']}\"'" for item in data])
+
+# Imprime la cadena de texto resultante
+print(bash_array)
